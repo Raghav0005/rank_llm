4a9c111d9a0e1a87f6acad24fe1e9edb44128163
Raghav Vasudeva <r2vasudeva@basilisk.cs.uwaterloo.ca>
2026-04-27 14:42:37 -0400

    Diver Pointwise Reranker

--- src/rank_llm/rerank/pointwise/__init__.py
+++ src/rank_llm/rerank/pointwise/__init__.py
@@ -8,10 +8,14 @@ try:
 except ImportError:
     MonoT5 = None
 
+try:
+    from .diver_reranker import DiverPointwiseReranker
+except ImportError:
+    DiverPointwiseReranker = None
 
 try:
     from .reason_embed_reranker import ReasonEmbedReranker
 except ImportError:
     ReasonEmbedReranker = None
 
-__all__ = ["MonoT5", "MonoELECTRA", "ReasonEmbedReranker"]
+__all__ = ["MonoT5", "MonoELECTRA", "DiverPointwiseReranker", "ReasonEmbedReranker"]
